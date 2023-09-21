c06f0361f9625dadb027fdb1947a198e547b0d34
自然自在 <omaidb@gmail.com>
2023-09-21 17:14:06 +0800

    sersync3

--- FileSynchronize.h
+++ FileSynchronize.h
@@ -1,18 +1,19 @@
-/* 
+/*
  * File:   ServersFileSysc.h
  * Author: root
  *
  * Created on 2009年12月21日, 下午1:29
  */
-#include<string>
-#include"Initialize.h"
-#include"Interface.h"
-#include"main.h"
+#include <string>
+#include "Initialize.h"
+#include "Interface.h"
+#include "main.h"
 #include <boost/shared_ptr.hpp>
 #ifndef _SERVERSFILESYSC_H
-#define	_SERVERSFILESYSC_H
+#define _SERVERSFILESYSC_H
 
-class FileSynchronize {
+class FileSynchronize
+{
 public:
     static std::string m_plugin;
     ptrInitialize m_init;
@@ -21,9 +22,9 @@ public:
     static std::string watch;
     static std::vector<ptrRmtServer> rmtServers;
     static int m_crontab;
-    static int sleep_group; //用于统计当前处于sleep状态的线程数量
-    static boost::condition_variable work_cond; //工作条件对象
-    static boost::mutex worklock; //用于统计工作线程数量的锁定
+    static int sleep_group;                     // 用于统计当前处于sleep状态的线程数量
+    static boost::condition_variable work_cond; // 工作条件对象
+    static boost::mutex worklock;               // 用于统计工作线程数量的锁定
     static std::string m_users;
     static std::string m_password;
     static std::string m_delete;
@@ -36,22 +37,21 @@ public:
     static bool debug;
     static string timeout;
     static string params;
+
 public:
     FileSynchronize(ptrInitialize init, ptrQFilter qf, ptrQRetry qr);
-    static int XmlParse(ptrInitialize init); 
+    static int XmlParse(ptrInitialize init);
     void ThreadAwaken();
-    static bool RsyncOnce();  
+    static bool RsyncOnce();
+
 protected:
-    static std::string StrEscaped(std::string);//escaped the '$' in the path
-    static std::string AddExclude(); 
-    static void ExecuteScript(std::string file_name); 
-    static void* ServerSyncThread(ptrQFilter qf, ptrQRetry qr, ptrInterface itf);
-    static void* RsyncThread(ptrQRetry qr, Event event, std::string ip, std::string module, std::string watch);
-    static void* QueueRetryThread(ptrQRetry qr);
+    static std::string StrEscaped(std::string); // escaped the '$' in the path
+    static std::string AddExclude();
+    static void ExecuteScript(std::string file_name);
+    static void *ServerSyncThread(ptrQFilter qf, ptrQRetry qr, ptrInterface itf);
+    static void *RsyncThread(ptrQRetry qr, Event event, std::string ip, std::string module, std::string watch);
+    static void *QueueRetryThread(ptrQRetry qr);
     static std::string DelPathCombine(std::string str, bool dir);
-
-
 };
 
-#endif	/* _SERVERSFILESYSC_H */
-
+#endif /* _SERVERSFILESYSC_H */
